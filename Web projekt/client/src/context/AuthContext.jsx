970b9bf360e25b784247e0eb44b4686018b56c19
Vanessa Vinkovic <vanessavinkovic2@gmail.com>
2026-08-31 22:34:41 +0200

    Dodan API_URL za backend na renderu

--- Web projekt/client/src/context/AuthContext.jsx
+++ Web projekt/client/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext(null);
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://backed-tty7.onrender.com/api";
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(() => {
